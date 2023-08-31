43a01f0aeb6836d5d06e994e687519505f40199d
Victor <pinkpolarbear@yandex.ru>
2023-08-31 21:01:19 +0300

    Update config.ts

--- server/config.ts
+++ server/config.ts
@@ -1,15 +1,15 @@
-const PORT = 5001
-const CACHE_TIMEOUT = 10000
-const REDIRECT_BASEURL = 'https://www.freetogame.com'
-const CHUNK_SIZE = 10
+const PORT = 3001;
+const CACHE_TIMEOUT = 10000;
+const REDIRECT_BASEURL = "https://www.freetogame.com";
+const CHUNK_SIZE = 10;
 
 export const API_PARAMS: Record<string, string> = {
-    '/api/games': '/api/games'
-}
+  "/api/games": "/api/games",
+};
 export default {
-    API_PARAMS,
-    CACHE_TIMEOUT,
-    CHUNK_SIZE,
-    PORT,
-    REDIRECT_BASEURL
-}
+  API_PARAMS,
+  CACHE_TIMEOUT,
+  CHUNK_SIZE,
+  PORT,
+  REDIRECT_BASEURL,
+};
